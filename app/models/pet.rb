db65e834c9544fc1ab8c055342d6ad108c7a0899
Adrian Simionescu <simionescuadrian@yahoo.com>
2022-02-23 16:39:50 +0400

    Changed the Active Record for Pet

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,7 +4,6 @@ class Pet < ApplicationRecord
   validates :age, presence: true
   validates :description, presence: true
   validates :price_per_day, presence: true
-  validates :breed, presence: true
   has_many :bookings
   belongs_to :breed
   belongs_to :species
