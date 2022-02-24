c9dbf681e6f0522c513dff9772b2bdead7878f1f
Shuja Khan <shuja3s@hotmail.com>
2022-02-24 17:04:57 +0400

    It can change the status of the bookings and each booking has a different section in the index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_02_24_091936) do
 
   # These are extensions that must be enabled in order to support this database
@@ -79,8 +78,6 @@ ActiveRecord::Schema.define(version: 2022_02_24_091936) do
     t.bigint "breed_id"
     t.bigint "user_id"
     t.bigint "species_id"
-    t.float "latitude"
-    t.float "longitude"
     t.index ["breed_id"], name: "index_pets_on_breed_id"
     t.index ["species_id"], name: "index_pets_on_species_id"
     t.index ["user_id"], name: "index_pets_on_user_id"
