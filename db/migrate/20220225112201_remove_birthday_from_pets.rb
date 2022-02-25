6213a223972fceed2b46d7036cc9d628f2778fa6
Shuja Khan <shuja3s@hotmail.com>
2022-02-25 15:55:10 +0400

    Two down, one to go

--- db/migrate/20220225112201_remove_birthday_from_pets.rb
+++
@@ -1,5 +0,0 @@
-class RemoveBirthdayFromPets < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :pets, :birthday, :date
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2022_02_25_100058) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,6 +69,7 @@ ActiveRecord::Schema.define(version: 2022_02_25_100058) do
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.string "gender"
+    t.integer "age"
     t.string "description"
     t.string "location"
     t.float "price_per_day"
@@ -82,21 +82,10 @@ ActiveRecord::Schema.define(version: 2022_02_25_100058) do
     t.bigint "species_id"
     t.float "latitude"
     t.float "longitude"
-    t.date "birthdaye"
-    t.date "birthday"
     t.date "birthdate"
     t.index ["breed_id"], name: "index_pets_on_breed_id"
     t.index ["species_id"], name: "index_pets_on_species_id"
     t.index ["user_id"], name: "index_pets_on_user_id"
-  end
-
-  create_table "pg_search_documents", force: :cascade do |t|
-    t.text "content"
-    t.string "searchable_type"
-    t.bigint "searchable_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -108,7 +97,6 @@ ActiveRecord::Schema.define(version: 2022_02_25_100058) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["booking_id"], name: "index_reviews_on_booking_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
-
   end
 
   create_table "species", force: :cascade do |t|
